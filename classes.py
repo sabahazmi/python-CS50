970a8827a137d04c1ae63242cb4ddaf33abf5908
sabahazmi <sabahazmi@live.com>
2020-08-22 16:12:31 +0530

    adding lamba and decorators

--- classes.py
+++ classes.py
@@ -3,18 +3,22 @@ class Flight():
         self.capacity = capacity
         self.passengers =  []
 
+# Adding passenger
     def add_passenger(self, name):
         if not self.open_seats():
             return False
         self.passengers.append(name)
         return True
 
+# Checking for seats availability
     def open_seats(self):
         return self.capacity - len(self.passengers)
 
+# Object of class Flight
 flight  = Flight(3)
 
 people = ["Harry", "Ron", "Hermione", "Ginny", "Luna"]
+
 for person in people:
     if flight.add_passenger(person):
         print(f"Added {person} to the flight successfully.")
